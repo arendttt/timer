924e0f2e24cdbe50bebac5b10bac5022e2a5de1a
Milena <mnarendt@rede.ulbra.br>
2024-08-24 14:47:54 -0300

    configuração estilos globais

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,9 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
 
+  /*
   ${props => {
     return css`
       background-color: ${buttonVariants[props.variant]}
     `
   }}
+    */
 `;
