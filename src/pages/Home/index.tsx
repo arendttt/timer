3686a51fd2621f381d227a6dc11523dcd579a5a4
Milena <mnarendt@rede.ulbra.br>
2024-12-15 16:31:09 -0300

    resetando os campos do formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -17,7 +17,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -27,6 +27,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) { // data: dados dos inputs
     console.log(data)
+    reset();
   };
 
   const task = watch('task'); // observar o campo
